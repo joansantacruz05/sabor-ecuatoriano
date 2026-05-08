255822b04c00b80ea8e44e31d10784ce62267780
ivaaansky <leoenelbara@gmail.com>
2026-05-08 00:17:04 -0500

    Update app.js

--- controller/app.js
+++ controller/app.js
@@ -5,16 +5,6 @@
 $(document).ready(function () {
 
 
-  // NOTIFICACIÓN SOLO UNA VEZ
-  if (!localStorage.getItem("notificacionMostrada")) {
-
-    // AQUÍ VA TU NOTIFICACIÓN
-    alert("🍽️ Bienvenido a Sabor Ecuatoriano");
-
-    // Guarda que ya fue mostrada
-    localStorage.setItem("notificacionMostrada", "si");
-  }
-
 
   
   // COOKIES - Banner
